8dc7e86454ee4398d8b7b0138d4aa1c1204d00e1
geekstyle <geekstylecn@gmail.com>
2019-01-03 17:53:57 +0800

    router partly completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HerosComponent }      from './heros/heros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heros', component: HerosComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
